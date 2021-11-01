5dfc9fbb7f63b98ddd1daafa5d2f6bdd181bdf9f
Rowan, Sarah <Sarah.Rowan@viasat.com>
2021-11-01 14:11:45 -0400

    adding submit alert feature (doesn't validate yet)

--- src/App.js
+++ src/App.js
@@ -18,22 +18,28 @@ function App() {
         errors.email = 'Field required'
       }
       else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Username should be an email';  
+        errors.email = 'Username should be an email';  //assignment required this error, even though the field is email, not username
       }    
       if(!values.password) errors.password = 'Field required'
       return errors;
     }
   });
+
+  //currently this works whenever Submit is clicked... it should only popup when Submit is clicked after passing the validations, but I can't figure that out
+  function handleSubmit(values) {
+   alert('Login Successful');
+  }
+
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
+      <form onSubmit={handleSubmit}>
       <div>Email</div>
         <input name="email" type="text" onChange={formik.handleChange} value={formik.values.name}/>
         {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div>: null}
         <div>Password</div>
         <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>
         {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div>: null}
-        <button type="submit">Submit</button>
+        <button id="submitBtn" type="submit">Submit</button>
       </form>
     </div>
   );
