c2f05f58dbb686651190bdb99dee443473c580f7
Rowan, Sarah <Sarah.Rowan@viasat.com>
2021-11-01 13:41:42 -0400

    added more based on assignment requirements -- still need handleSubmit functionality

--- src/App.js
+++ src/App.js
@@ -14,20 +14,25 @@ function App() {
     },
     validate: values =>{
       let errors = {};
-      if(!values.email) errors.email = 'Email is required'
-      if(!values.password) errors.password = 'Password is required';
+      if(!values.email) {
+        errors.email = 'Field required'
+      }
+      else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Username should be an email';  
+      }    
+      if(!values.password) errors.password = 'Field required'
       return errors;
     }
   });
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <div>Email</div>
+      <div>Email</div>
         <input name="email" type="text" onChange={formik.handleChange} value={formik.values.name}/>
-        {formik.errors.email ? <div style={{color:'red'}}>{formik.errors.email}</div>: null}
+        {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div>: null}
         <div>Password</div>
         <input name="password" type="text" onChange={formik.handleChange} value={formik.values.password}/>
-        {formik.errors.password ? <div style={{color:'red'}}>{formik.errors.password}</div>: null}
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div>: null}
         <button type="submit">Submit</button>
       </form>
     </div>
